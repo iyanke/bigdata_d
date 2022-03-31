676a88d94edd155651b5cbf00a639cc6049cebd2
admin <admin@TeamCity>
2022-03-31 16:00:31 +0300

    TeamCity change in 'BigData' project: build features of 'eclipse' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.testsSplit
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
@@ -48,6 +49,12 @@ object Eclipse : BuildType({
             goals = "clean test"
             pomLocation = "java_eclipse/pom.xml"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+
+    features {
+        testsSplit {
+            numberOfParts = 2
         }
     }
 })
